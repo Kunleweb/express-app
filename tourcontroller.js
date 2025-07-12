9b6b6f81f9ecd20b1747552d3ee72f9c24fbf820
Kunleweb <booment11@gmail.com>
2025-07-12 23:49:56 +0100

    advanced filtering check

--- tourcontroller.js
+++ tourcontroller.js
@@ -5,10 +5,36 @@ const Tour = require('./models/tourmodels')
 
 
 exports.getalltours = async (req,res)=>{
-    const query = Tour.find()
+
+    // Basic Filtering
+    // const queryOBJ = {...req.url}
+    const query = Tour.find(queryOBJ).split(',').join(' ')
+
+
+    // Advanced Filtering
+
+    // let querystr = JSON.stringify(req.query)
+    // querystr = querystr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
+    // req.query = req.query.find(JSON.parse(querystr))
+
+
+     
+
+
+
+
+
+
+
+
+    // Execute
     const tour = await query
     res.status(200).json({status:'success', results: tour.length, data:{tour}})
 }
+
+
+
+// do filter, sort, limit, pagination, 
 
 
 exports.gettours = async (req,res)=>{
