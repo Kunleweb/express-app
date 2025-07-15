6ab7c7b551970b63779253da26423c102b08accb
Kunleweb <booment11@gmail.com>
2025-07-15 01:16:47 +0100

    API feature refactoring

--- tourcontroller.js
+++ tourcontroller.js
@@ -1,103 +1,125 @@
+const Tour = require("./models/tourmodels");
 
-const Tour = require('./models/tourmodels')
+exports.aliastoptours = (req, res, next) => {
+  req.query.limit = "5";
+  req.query.sort = "-price";
+  next();
+};
 
+// Refactoring our API features
 
-exports.aliastoptours = (req, res, next) =>{
-    req.query.limit = '5';
-    req.query.sort = '-price';
-    next()
-}
+class APIfeatures{
+  constructor(query, queryString) {
+    this.query = query, 
+    this.queryString = queryString
+  }
 
-
-
-
-
-
-
-
-exports.getalltours = async (req,res)=>{
-
-    // Basic Filtering: Here we exclude params from the request query
-    const queryObj = {...req.query};
-    const excludedFields = ["page", "sort", "limit", "fields"];  
-    excludedFields.forEach((el) => delete queryObj[el]); 
+  filter() {
+    const queryObj = { ...this.queryString };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((el) => delete queryObj[el]);
     // Basic Filtering: Here we filter params from the request query
 
-    
     // Advanced filtering in other to add more keys to params
-    let queryStr = JSON.stringify(queryObj)
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
-    let query = Tour.find(JSON.parse(queryStr))
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
 
-
-    // Sorting based on field ?sort=price
-    if (req.query.sort){
-        const sortby = req.query.sort.split(',').join(' ');
-        query= query.sort(sortby)
-    }else{
-        query =query.sort('-createdAt')
+  sort() {
+    if (this.queryString.sort) {
+      const sortby = this.queryString.sort.split(",").join(" ");
+      this.query = this.query.sort(sortby);
+    } else {
+      this.query = this.query.sort("-createdAt");
     }
 
+    return this;
+  }
 
-    // Limiting: ?fields =  basically to decide which data is shown to us.
-    if(req.query.fields){
-        const fields = req.query.fields.split(',').join(' ');
-        query = query.select(fields)
+  limit() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(",").join(" ");
+      this.query = this.query.select(fields);
     }
+    return this;
+  }
 
-
-
-    // Pagination
-    // specify ?page and limit
-
-    if (req.query.page){
-        const page = req.query.page*1 || 1
-        const limit = req.query.limit*1 || 100
-        const skip = (page-1)*limit
-        query=query.skip(skip).limit(limit)
-
-
-    }
-
-
-
-
-
-
-
-
-    const tour = await query
-
-
-
-
-
-
-    res.status(200).json({status:'success', results: tour.length, data:{tour}})
+  pagination() {
+   
+      const page = this.queryString.page * 1 || 1;
+      const limit = this.queryString.limit * 1 || 100;
+      const skip = (page - 1) * limit;
+      this.query = this.query.skip(skip).limit(limit);
+    
+    return this;
+  }
 }
 
+exports.getalltours = async (req, res) => {
+  // // Basic Filtering: Here we exclude params from the request query
+  // const queryObj = {...req.query};
+  // const excludedFields = ["page", "sort", "limit", "fields"];
+  // excludedFields.forEach((el) => delete queryObj[el]);
+  // // Basic Filtering: Here we filter params from the request query
 
-exports.gettours = async (req,res)=>{
-    const tour = await Tour.findById(req.params.id);
-    res.status(200).json({status:'sucess', data:{tour}})
-}
+  // // Advanced filtering in other to add more keys to params
+  // let queryStr = JSON.stringify(queryObj)
+  // queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
+  // let query = Tour.find(JSON.parse(queryStr))
 
+  // // Sorting based on field ?sort=price
+  // if (req.query.sort){
+  //     const sortby = req.query.sort.split(',').join(' ');
+  //     query= query.sort(sortby)
+  // }else{
+  //     query =query.sort('-createdAt')
+  // }
 
-exports.createtour= (req,res)=>{
-    const new_ID = tours[tours.length-1].id+1;
-    const new_tour = Object.assign({id:new_ID}, req.body);
-    tours.push(new_tour);
-    fs.writeFile(`./tours-simple.json`, JSON.stringify(tours), (err)=>{
-    res.status(201).json({status:'success', data:{tours:new_tour}})})
-}
+  // // Limiting: ?fields =  basically to decide which data is shown to us.
+  // if(req.query.fields){
+  //     const fields = req.query.fields.split(',').join(' ');
+  //     query = query.select(fields)
+  // }
 
+  // // Pagination
+  // // specify ?page and limit
 
+  // if (req.query.page){
+  //     const page = req.query.page*1 || 1
+  //     const limit = req.query.limit*1 || 100
+  //     const skip = (page-1)*limit
+  //     query=query.skip(skip).limit(limit)
 
-exports.deletetour = async(req,res) =>{
-    try{const tour = await Tour.findOneAndDelete({ _id: id })
-}
-    catch(err){res.status(404).json({status:'fail', message: err})}
+  // }
 
-}
+  const features = new APIfeatures(Tour.find(), req.query).filter().sort().limit().pagination();
+  const tours = await features.query;
 
+  res
+    .status(200)
+    .json({ status: "success", results: tours.length, data: tours });
+};
 
+exports.gettours = async (req, res) => {
+  const tour = await Tour.findById(req.params.id);
+  res.status(200).json({ status: "sucess", data: { tour } });
+};
+
+exports.createtour = (req, res) => {
+  const new_ID = tours[tours.length - 1].id + 1;
+  const new_tour = Object.assign({ id: new_ID }, req.body);
+  tours.push(new_tour);
+  fs.writeFile(`./tours-simple.json`, JSON.stringify(tours), (err) => {
+    res.status(201).json({ status: "success", data: { tours: new_tour } });
+  });
+};
+
+exports.deletetour = async (req, res) => {
+  try {
+    const tour = await Tour.findOneAndDelete({ _id: id });
+  } catch (err) {
+    res.status(404).json({ status: "fail", message: err });
+  }
+};
